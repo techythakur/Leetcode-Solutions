dae6e48d84ea0b996976b26eff7288b5ee390fe7
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2022-11-17 15:01:13 +0530

    Time: 118 ms (32.31%), Space: 14 MB (26.26%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.py
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -1,7 +1,20 @@
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        def area(x1,y1,x2,y2):
+        widthA = ax2 - ax1
+        heightA = ay2 - ay1
+        areaA = widthA * heightA
+
+        widthB = bx2 - bx1
+        heightB = by2 - by1
+        areaB = widthB * heightB
+        
+        widthO = max(0, min(ay2, by2) - max(ay1, by1))
+        heightO = max(0, min(ax2, bx2) - max(ax1, bx1))
+        areaO = widthO * heightO
+        
+        return areaA + areaB - areaO
+        '''def area(x1,y1,x2,y2):
             return (x2-x1)*(y2-y1)
         xOverlap = max(min(ax2,bx2)-max(ax1,bx1), 0)
         yOverlap = max(min(ay2,by2)-max(ay1,by1), 0)
-        return area(ax1,ay1,ax2,ay2) + area(bx1,by1,bx2,by2) - xOverlap*yOverlap
+        return area(ax1,ay1,ax2,ay2) + area(bx1,by1,bx2,by2) - xOverlap*yOverlap'''
