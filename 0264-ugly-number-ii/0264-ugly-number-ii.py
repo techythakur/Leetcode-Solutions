eb19067d5448944f98056397af8510dbecef6fff
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2022-11-18 10:52:20 +0530

    Time: 724 ms (17.45%), Space: 13.8 MB (86.09%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,13 +1,27 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        ugly=[1]
+        nums=[1]
+        i2 = i3 = i5 = 0
+
+        for i in range(1, 1690):
+            ugly = min(nums[i2] * 2, nums[i3] * 3, nums[i5] * 5)
+            nums.append(ugly)
+
+            if ugly == nums[i2] * 2: 
+                i2 += 1
+            if ugly == nums[i3] * 3:
+                i3 += 1
+            if ugly == nums[i5] * 5:
+                i5 += 1
+        return nums[n-1]
+        '''ugly=[1]
         two=three=five=0
         while len(ugly)<n:
             while ugly[two]*2<=ugly[-1]: two+=1
             while ugly[three]*3<=ugly[-1]: three+=1
             while ugly[five]*5<=ugly[-1]: five+=1
             ugly.append(min(ugly[two]*2,ugly[three]*3,ugly[five]*5))
-        return ugly[-1]
+        return ugly[-1]'''
         
         '''arr=[]
         cnt=0
