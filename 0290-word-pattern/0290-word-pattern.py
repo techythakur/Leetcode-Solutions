24e1a19e9faf4865b25da6f6724403f337a04c83
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2022-11-22 16:43:56 +0530

    Time: 32 ms (91.84%), Space: 13.9 MB (75.95%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,10 +1,10 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        arr = s.split()
-        if len(arr) != len(pattern):
+        arr=s.split()
+        if len(arr)!=len(pattern):
             return False
         
-        for i in range(len(arr)):
-            if pattern.find(pattern[i]) != arr.index(arr[i]):
+        for i in range(len(pattern)):
+            if pattern.find(pattern[i])!=arr.index(arr[i]):
                 return False
         return True
