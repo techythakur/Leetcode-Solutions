a36d2d26d3187f12e00ada448c7666bb4d94ea7a
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2022-11-22 16:25:46 +0530

    Time: 1496 ms (52.05%), Space: 27.2 MB (55.48%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        x = {}
+        x={}
+        
         for i in range(len(nums)):
             if nums[i] in x:
-                if i - x[nums[i]] <= k:
+                if i-x[nums[i]]<=k:
                     return True
                 else:
-                    x[nums[i]] = i
+                    x[nums[i]]=i
             else:
-                x[nums[i]] = i
-                
+                x[nums[i]]=i
         return False
