5bf625971e7b62eb54064d7cfe263ea47d73a96b
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2022-12-21 00:25:30 +0530

    Time: 148 ms (42.15%), Space: 14.4 MB (57.55%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        n, keys, open = len(rooms), deque([0]), set()
+        
+        while keys and len(open) < n:
+            key = keys.popleft()
+            open.add(key)
+            
+            for r in rooms[key]:
+                if r not in open:
+                    keys.append(r)
+                    
+        return len(open) == n
