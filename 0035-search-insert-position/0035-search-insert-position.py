0e6e4683c7f59782b5cab5c66abe17013706ace5
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2023-02-20 22:11:15 +0530

    Time: 54 ms (55.67%), Space: 14.6 MB (98.62%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i,j=0,len(nums)-1
+        while i<=j:
+            mid=(i+j)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                j=mid-1
+            else:
+                i=mid+1
+        return i
