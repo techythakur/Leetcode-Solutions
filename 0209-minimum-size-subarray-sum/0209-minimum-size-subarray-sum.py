2a04907d2f589b509a9ac00db0be133437e7e247
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2024-11-23 21:52:19 +0530

    Time: 7 ms (93.12%), Space: 27.4 MB (82.03%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,20 +1,37 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        if sum(nums)<target:
-            return 0
-        minm = len(nums)
-        l,r=0,0
-        summ = 0
-        while r<len(nums):
-            if nums[l]>=target or nums[r]>=target:
-                return 1
-            summ += nums[r]
-            r+=1
-            if summ>=target:
-                while l<=r and summ>=target:
-                    summ-=nums[l]
-                    l+=1
-                minm = min(r-l+1,minm)            
-        return minm
+        minm = float('inf')
+        curr = 0
+        left = 0
+        for right in range(len(nums)):
+            curr += nums[right]
+            
+            while curr>=target:
+                if right-left+1<minm:
+                    minm = right-left+1
+                
+                curr-=nums[left]
+                left+=1
+        return minm if minm!=float('inf') else 0
+        
+        
+        
+        
+        # if sum(nums)<target:
+        #     return 0
+        # minm = len(nums)
+        # l,r=0,0
+        # summ = 0
+        # while r<len(nums):
+        #     if nums[l]>=target or nums[r]>=target:
+        #         return 1
+        #     summ += nums[r]
+        #     r+=1
+        #     if summ>=target:
+        #         while l<=r and summ>=target:
+        #             summ-=nums[l]
+        #             l+=1
+        #         minm = min(r-l+1,minm)            
+        # return minm
                 
             
