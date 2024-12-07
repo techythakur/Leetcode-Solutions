eed5b6eb5b62ff9f7ef90990565b88d56d764d3f
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2024-12-08 01:47:55 +0530

    Time: 0 ms (100.00%), Space: 17.3 MB (11.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,22 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i,j,k = m-1, n-1, (m+n)-1    # take 3 pointers to i points to nums1, j points to nums2, k points to end of nums1 i.e. (m+n)
-        
-        while i>=0 and j>=0:
-            if nums1[i]>=nums2[j]:  # compare which is bigger and place it to end of nums1 simultaneously
-                nums1[k] = nums1[i]
-                i-=1
-            else:
-                nums1[k] = nums2[j]
-                j-=1
-            k-=1
-        
-        while j>=0:    # if still some elements are left in nums2, place all of them in nums1
-            nums1[k]=nums2[j]
-            j-=1
-            k-=1
-            
+        for i in range(n):
+            nums1[m+i] = nums2[i]
+        nums1.sort()
         return nums1
-        
         
