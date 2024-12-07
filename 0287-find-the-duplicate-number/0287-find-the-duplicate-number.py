efde7b07dff39f3faf4faf3999b5958d98c0e097
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2024-12-08 03:07:35 +0530

    Time: 26 ms (64.83%), Space: 29 MB (38.65%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,9 +1,22 @@
-from collections import Counter
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        s = set()
-        for i in nums:
-            if i in s:
-                return i
-            else:
-                s.add(i)
+        # tortoise and hare's algo or Floyds cycle finding algo
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:    # detects the cycle in the array
+                break
+        fast = nums[0]
+        while slow != fast:    # get fast to the duplicate array by resetting fast
+            slow = nums[slow]
+            fast = nums[fast]
+        return fast
+    
+        # s = set()
+        # for i in nums:
+        #     if i in s:
+        #         return i
+        #     s.add(i)
+        # return False
