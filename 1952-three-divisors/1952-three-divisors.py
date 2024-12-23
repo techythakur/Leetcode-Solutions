a8daaa234f387102cc1c8731ad265fcd487b6142
Aman Kumar Thakur <65844095+techythakur@users.noreply.github.com>
2024-12-24 00:05:50 +0530

    Time: 4 ms (37.55%), Space: 18 MB (7.01%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        count = 0
+        for i in range(1,n+1):
+            if n%i ==0:
+                count+=1
+        return True if count==3 else False
